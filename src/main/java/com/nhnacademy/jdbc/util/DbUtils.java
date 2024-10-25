3ad437758c7b2cfae44e8d0e9c54a9847f640496
이동수 <dongsu6666@naver.com>
2024-10-25 09:46:56 +0900

    clear

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -14,9 +14,9 @@ public class DbUtils {
     static {
         BasicDataSource basicDataSource = new BasicDataSource();
         //todo#0 {ip},{database},{username},{password} 설정 합니다.
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_32");
+        basicDataSource.setUsername("nhn_academy_32");
+        basicDataSource.setPassword("EJdrW!(bf]HjT9a5");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
